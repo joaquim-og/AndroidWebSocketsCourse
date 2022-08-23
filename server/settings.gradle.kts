c5f6e5274bde7e4fdae345e47094bb76d8f67a14
Joaquim Gomes <gomes@devgrid.co.uk>
2022-08-22 21:38:13 -0300

    first commit

---
+++ server/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.androidWebSocketServerGameCourse.android-websocket-server"
---
+++ server/src/main/kotlin/com/androidWebSocketServerGameCourse/Application.kt
@@ -0,0 +1,14 @@
+package com.androidWebSocketServerGameCourse
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.androidWebSocketServerGameCourse.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureSerialization()
+        configureSockets()
+        configureMonitoring()
+        configureRouting()
+    }.start(wait = true)
+}
